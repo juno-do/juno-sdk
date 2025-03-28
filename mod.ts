5eec897d9f7cb1efdf64f92f675861261d3d1cf2
sergio Martin Sanchez <martinseec1@gmail.com>
2025-03-28 13:00:12 +0100

    Update the deno.json configuration and improve error handling in mod.ts, allowing functions to return null instead of throwing errors. Compiler options are added, and types are exported from mod.ts.
    Signed-off-by: sergio Martin Sanchez <martinseec1@gmail.com>

--- mod.ts
+++ mod.ts
@@ -1,20 +1,20 @@
 import { Element, FilterOptions, ListTypeReturn } from "./types.ts";
+export type { Element, FilterOptions, ListTypeReturn };
 
 function getValueFromArgs(name: string, type: "parameters" | "secrets") {
   const prefix = `--${type}=`;
   const args: string[] = Deno.args;
   const payloadUnDecoded = args.find((arg) => arg.includes(prefix));
   if (!payloadUnDecoded) {
-    throw new Error(`No ${type}`);
+    return null;
   }
   const payload = payloadUnDecoded.replace(prefix, "");
   const payloadDecoded = atob(payload);
-  const payloadJson: { name: string; value: string }[] = JSON.parse(
-    payloadDecoded,
-  );
+  const payloadJson: { name: string; value: string }[] =
+    JSON.parse(payloadDecoded);
   const value = payloadJson.find((p) => p.name === name);
   if (!value) {
-    throw new Error("No value");
+    return null;
   }
   return value?.value;
 }
@@ -24,11 +24,11 @@ function getValueFromArgs(name: string, type: "parameters" | "secrets") {
  * podras obtener el valor de los parametros que has definido en el manifest
  * @param name parameter name
  */
-export function getParameterValue(name: string) {
+export function getParameterValue(name: string): string | null {
   return getValueFromArgs(name, "parameters");
 }
 
-export function getSecretValue(key: string) {
+export function getSecretValue(key: string): string | null {
   return getValueFromArgs(key, "secrets");
 }
 
@@ -79,20 +79,19 @@ export function returnOneItemResponse(response: Element) {
  */
 export async function junoFetch(
   input: URL | Request | string,
-  init?: RequestInit,
+  init?: RequestInit
 ): Promise<Response> {
   const PROXY_URL = Deno.env.get("PROXY_URL");
   if (!PROXY_URL) {
     throw new Error("No proxy url");
   }
-   const response = await fetch(`http://${PROXY_URL}`, {
+  const response = await fetch(`http://${PROXY_URL}`, {
     ...init,
     headers: {
       ...init?.headers,
       "Content-Type": "application/json",
-      "X-Juno-token-encrypted": getSecretValue(
-        "USER_INTEGRATION_TOKEN_ENCRYPTED",
-      ),
+      "X-Juno-token-encrypted":
+        getSecretValue("USER_INTEGRATION_TOKEN_ENCRYPTED") ?? "",
       "X-Juno-input": input.toString(),
     },
   });
